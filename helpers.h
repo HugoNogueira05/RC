0d1c28ab0271fef8848211a8079799ff85382ec9
HugoNogueira05 <hugonogueira055@gmail.com>
2025-10-09 14:57:04 +0100

    Initial working version modular

---
+++ helpers.h
@@ -0,0 +1,34 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include "macros.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Open and configure the serial port.
+// Returns -1 on error.
+int openSerialPort(const char *serialPort, int baudRate);
+
+// Restore original port settings and close the serial port.
+// Returns 0 on success and -1 on error.
+int closeSerialPort();
+
+// Wait up to 0.1 second (VTIME) for a byte received from the serial port.
+// Must check whether a byte was actually received from the return value.
+// Save the received byte in the "byte" pointer.
+// Returns -1 on error, 0 if no byte was received, 1 if a byte was received.
+int readByteSerialPort(unsigned char *byte);
+
+// Write up to numBytes from the "bytes" array to the serial port.
+// Must check how many were actually written in the return value.
+// Returns -1 on error, otherwise the number of bytes written.
+int writeBytesSerialPort(const unsigned char *bytes, int nBytes);
+
+
+#endif
---
+++ macros.h
@@ -0,0 +1,18 @@
+#ifndef   MACROS_H
+#define   MACROS_H
+
+// basic constants
+#define  FALSE   0
+#define  TRUE    1
+
+// frame characters
+#define FLAG 0X7E
+
+// serial port config
+#define _POSIX_SOURCE 1 // POSIX compliant source
+#define BAUDRATE 38400
+#define BUF_SIZE 256
+
+
+
+#endif
