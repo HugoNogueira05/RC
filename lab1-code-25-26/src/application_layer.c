022d45aaf2a8d9d7a63c1eb45818617ec38f6237
silvarui9119@gmail.com <silvarui9119@gmail.com>
2025-10-16 15:59:01 +0100

    added llopen

--- lab1-code-25-26/src/application_layer.c
+++ lab1-code-25-26/src/application_layer.c
@@ -1,18 +1,21 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
-
+#include "link_layer.h"
 #include <stdio.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
     // TODO: Implement this function
-    llopen({
-        .baudRate = baudRate,
-        .timeout = timeout,
-        .nRetransmissions = nTries,
-        .serialPort = serialPort,
-        .role = (strcmp(role, "tx") == 0) ? LlTx : LlRx
-    });
+    LinkLayer connectionParameters;
+    connectionParameters.baudRate = baudRate;
+    connectionParameters.nRetransmissions = nTries;
+    connectionParameters.timeout = timeout;
+    strncpy(connectionParameters.serialPort, serialPort, 50);
+    if (strcmp(role, "tx") == 0)
+        connectionParameters.role = LlTx;
+    else if (strcmp(role, "rx") == 0)
+        connectionParameters.role = LlRx;
+    llopen(connectionParameters);
 }
